284fb88573a51c6683904d9033573bbb4a69e82b
Gustavo Decker Zambrano <gustavdz@gmail.com>
2022-05-01 15:13:22 -0500

    test(movement): test if the rover is out of bounds

--- server/tests/movement.spec.js
+++ server/tests/movement.spec.js
@@ -2,6 +2,8 @@ const {
   createRover,
   moveRover,
   getNewLocation,
+  getBounds,
+  checkBounds,
 } = require("../service/movementService");
 
 describe("Result as expected", () => {
@@ -15,5 +17,21 @@ describe("Result as expected", () => {
     const newLocation = getNewLocation(movedRover);
 
     expect(newLocation).toEqual(resultExpected);
+  });
+});
+
+describe("Rover out of bound", () => {
+  it("5 5 - 1 2 N - LMMMMMLMLMLMM", () => {
+    const bounds = "5 5";
+    const currentLocation = "1 2 N";
+    const movement = "LMMMMMLMLMLMM";
+    const resultExpected = false;
+
+    const coords = getBounds(bounds);
+    const rover = createRover(currentLocation);
+    const movedRover = moveRover(movement, rover);
+    const inBounds = checkBounds(movedRover, coords);
+
+    expect(inBounds).toEqual(resultExpected);
   });
 });

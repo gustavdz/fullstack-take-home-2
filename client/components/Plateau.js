8573671067fd1b2595de137c4cb937cba7863214
Gustavo Decker Zambrano <gustavdz@gmail.com>
2022-05-01 17:58:02 -0500

    fix(rover): control 0 as no falsy

--- client/components/Plateau.js
+++ client/components/Plateau.js
@@ -1,8 +1,15 @@
 import React, { useEffect } from "react";
 import { styled } from "@mui/material/styles";
+import { keyframes } from "@mui/system";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
+import { Typography } from "@mui/material";
+
+const blink = keyframes`
+  from { opacity: 0; }
+  to { opacity: 1; }
+`;
 
 const Coord = styled(Paper)(({ theme }) => ({
   backgroundColor: "#1A2027",
@@ -10,6 +17,10 @@ const Coord = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(0),
   textAlign: "center",
   color: "#76ff03",
+}));
+
+const BlinkingText = styled(Typography)(({ theme }) => ({
+  animation: `${blink} 1s linear infinite`,
 }));
 
 function Position(props) {
@@ -41,25 +52,23 @@ function Position(props) {
                     {rover &&
                     rover.orientation &&
                     rover.orientation !== "" &&
-                    rover.position?.x &&
-                    rover.position?.y &&
-                    rover.position.x === xPosition &&
-                    rover.position.y === yPosition ? (
+                    ![null, "", false].includes(rover.position?.x) &&
+                    ![null, "", false].includes(rover.position?.y) &&
+                    rover.position.x + 1 === xPosition + 1 &&
+                    rover.position.y + 1 === yPosition + 1 ? (
                       <Coord>
-                        {rover.orientation === "N" ? (
-                          "^"
-                        ) : rover.orientation === "S" ? (
-                          "V"
-                        ) : rover.orientation === "E" ? (
-                          ">"
-                        ) : rover.orientation === "O" ? (
-                          "<"
-                        ) : (
-                          <Coord> {`${xPosition}${yPosition}`}</Coord>
-                        )}
+                        <BlinkingText>
+                          {rover.orientation === "N"
+                            ? "^"
+                            : rover.orientation === "S"
+                            ? "V"
+                            : rover.orientation === "E"
+                            ? ">"
+                            : "<"}
+                        </BlinkingText>
                       </Coord>
                     ) : (
-                      <Coord> {`${xPosition}${yPosition}`}</Coord>
+                      <Coord>{`coord(${xPosition},${yPosition})`}</Coord>
                     )}
                   </Grid>
                 ))}
